9e46c5e63ead1dcc2efe992cf199abc2ebbb828c
bishalbar77 <bishalranabr7@gmail.com>
2022-07-09 00:28:26 +0530

    homepage udpate update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,8 +9,6 @@
       <div class="row">
         <div class="col-lg-8 col-md-10 mx-auto">
           <div class="site-heading">
-            <h1>21 Twelve</h1>
-            <span class="subheading">21 Twelve</span>
           </div>
         </div>
       </div>
